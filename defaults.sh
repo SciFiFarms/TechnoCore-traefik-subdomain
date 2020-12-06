4a38bb73a6a2093791853a99a95b6fb292cc648b
Spencer Hachmeister <tms@spencerslab.com>
2020-12-06 16:53:07 -0700

    Remove trailing \ from TRAEFIK_DOMAINS env value.

--- defaults.sh
+++ defaults.sh
@@ -21,7 +21,7 @@ if echo "$DNS_PROVIDER" | grep -i "duckdns" > /dev/null; then
     export INGRESS_DNS_TOKEN_ENV_NAME=DUCKDNS_TOKEN
     export TRAEFIK_LETS_ENCRYPT_CHALLENGE="--acme.dnsChallenge.provider=duckdns "
     export SECRETS_TO_IGNORE=ingress_dns_user,$SECRETS_TO_IGNORE
-    export TRAEFIK_DOMAINS="--acme.domains=\"*.${DOMAIN},${DOMAIN}\" " \
+    export TRAEFIK_DOMAINS="--acme.domains=\"*.${DOMAIN},${DOMAIN}\" " 
     export SECRETS_TO_IGNORE
 
 elif echo "$DNS_PROVIDER" | grep -i "cloudflare" > /dev/null; then
@@ -29,7 +29,7 @@ elif echo "$DNS_PROVIDER" | grep -i "cloudflare" > /dev/null; then
     export INGRESS_DNS_USER_ENV_NAME=CLOUDFLARE_EMAIL
     export INGRESS_DNS_TOKEN_ENV_NAME=CLOUDFLARE_API_KEY
     export TRAEFIK_LETS_ENCRYPT_CHALLENGE="--acme.dnsChallenge.provider=cloudflare "
-    export TRAEFIK_DOMAINS="--acme.domains=\"*.${DOMAIN},${DOMAIN}\" " \
+    export TRAEFIK_DOMAINS="--acme.domains=\"*.${DOMAIN},${DOMAIN}\" " 
 
 #elif echo "$DNS_PROVIDER" | grep -i ""; then135.181.110.238
 #    echo ""
